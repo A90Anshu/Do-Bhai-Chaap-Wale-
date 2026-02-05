e9dd5652dde548a16e99278244bb4dd3fe00a302
A90Anshu <dram9386@gmail.com>
2026-02-05 22:14:57 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,20 +1,31 @@
-importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js");
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js');
 
-firebase.initializeApp({
-  apiKey: "PASTE SAME API KEY FROM admin.html",
-  authDomain: "PASTE SAME AUTH DOMAIN",
-  projectId: "PASTE SAME PROJECT ID",
-  messagingSenderId: "PASTE SAME SENDER ID",
-  appId: "PASTE SAME APP ID"
-});
+const firebaseConfig = {
+  apiKey: "AIzaSyADDoSv7Wt8WOfYkcAYZcQOZtCKROuHMu4",
+  authDomain: "dobhaichaapwale1.firebaseapp.com",
+  projectId: "dobhaichaapwale1",
+  storageBucket: "dobhaichaapwale1.firebasestorage.app",
+  messagingSenderId: "141034963645",
+  appId: "1:141034963645:web:442a93b6e87643b3bf2dd4"
+};
 
+firebase.initializeApp(firebaseConfig);
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(function(payload) {
-  self.registration.showNotification("🚨 NEW ORDER", {
-    body: "Open admin panel",
-    vibrate: [500, 500, 500, 500],
-    requireInteraction: true
-  });
+messaging.onBackgroundMessage((payload) => {
+  const notificationTitle = '🔔 New Order!';
+  const notificationOptions = {
+    body: 'Open Admin Panel to accept.',
+    icon: 'https://cdn-icons-png.flaticon.com/512/747/747376.png', // Standard food icon
+    vibrate: [200, 100, 200],
+    data: { url: '/pf.html' } // Click to open admin
+  };
+
+  return self.registration.showNotification(notificationTitle, notificationOptions);
+});
+
+self.addEventListener('notificationclick', function(event) {
+  event.notification.close();
+  event.waitUntil(clients.openWindow(event.notification.data.url));
 });
